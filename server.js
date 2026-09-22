c2839b0c5e261a497720cda09ea1155b1d6bc9ae
henrymvungi20-web <henrymvungi20@gmail.com>
2026-09-22 06:35:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,18 +3,24 @@ const express = require('express');
 const cors = require('cors');
 const MetaApi = require('metaapi.cloud-sdk').default;
 
-
 const app = express();
+
+// Parse incoming JSON payloads (Fixes req.body undefined error)
+app.use(express.json());
+app.use(cors());
+
+// Custom CORS Headers Middleware
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "*");
-  
+
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
   }
   next();
 });
+
 
 
 
